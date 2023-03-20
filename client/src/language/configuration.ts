f162218b01c852cd7f208110906c04245c368157
oceans <oceans-mail@qq.com>
2023-03-20 17:26:39 +0800

    Fix: comment bug

--- client/src/language/configuration.ts
+++ client/src/language/configuration.ts
@@ -3,7 +3,7 @@ import { CharacterPair, languages, TextEditor } from "vscode";
 import * as CONSTANT from "../constants";
 
 export function setLanguageConfiguration(activeTextEditor: TextEditor): void {
-	let pair: CharacterPair = ["{%*", "%*}"];
+	let pair: CharacterPair = ["{%*", "*%}"];
 
 	languages.setLanguageConfiguration(CONSTANT.languageId, {
 		comments: { blockComment: pair }
